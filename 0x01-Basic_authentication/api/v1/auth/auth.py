7f21fa303abd7e4a0a4491f69bafca2e9ea27156
mosehmaguah <mosehmaguah@gmail.com>
2023-11-07 07:05:09 -0800

    basic api authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" class to manage the API authentication """
-
+""" Auth class, Require auth with stars """
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth():
-    """ manages the API authentication """
+    """ manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require authorithation """
         if path is None or excluded_paths is None or not len(excluded_paths):
